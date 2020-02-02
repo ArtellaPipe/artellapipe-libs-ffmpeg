b5f21b7dbe2ac2df1de4c8bb72ece7dcba0f36fb
Tomi Poveda <tpovedatd@gmail.com>
2020-02-02 16:20:04 +0100

    Removed PIL dependency for Maya

--- artellapipe/libs/ffmpeg/__init__.py
+++ artellapipe/libs/ffmpeg/__init__.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import logging
 
 LOGGER = logging.getLogger()
@@ -28,38 +27,3 @@ def get_ffmpeg_executable():
         return None
 
     return ffmpeg_exe_path
-
-
-def get_pil_lib():
-    """
-    Returns path where PIL lib for current DCC is located
-    :return: str
-    """
-
-    import tpDccLib as tp
-    from tpPyUtils import path
-
-    externals_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'externals', 'lib')
-    if not externals_dir or not os.path.isdir(externals_dir):
-        LOGGER.warning('No externals directory found: "{}"'.format(externals_dir))
-        return
-
-    dcc_name = tp.Dcc.get_name()
-    dcc_version = tp.Dcc.get_version_name()
-
-    pil_lib_path = path.clean_path(os.path.join(externals_dir, dcc_name, dcc_version, 'pil'))
-    if not os.path.isdir(pil_lib_path):
-        LOGGER.warning('No PIL library directory found in path: "{}"'.format(pil_lib_path))
-        return None
-
-    return pil_lib_path
-
-
-# We add PIL to sys.path if necessary ...
-try:
-    import PIL
-except ImportError:
-    pil_path = get_pil_lib()
-    if pil_path and os.path.isdir(pil_path) and not pil_path in sys.path:
-        LOGGER.debug('Adding PIL path to sys.path: {} ...'.format(pil_path))
-        sys.path.append(pil_path)
